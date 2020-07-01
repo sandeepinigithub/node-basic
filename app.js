56c5f6b3b87967573825a2170b57df46ae9ecbc3
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2020-07-02 01:30:27 +0530

    Seventh Commit - ObjectDestructuring

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// console.log("PROCESS :",process);
-const helpers = require("./helpers");
-//var let const
-const total = helpers.sum(10 ,20);
+const {sum} = require("./helpers");
+
+const total = sum(10 ,20); //earlier it was helpers.sum(10,20)
 console.log("TOTAL :",total);
 
--- helpers.js
+++ helpers.js
@@ -1,10 +1,1 @@
-// function sum(a, b){
-//     return a + b;
-// }
-
 exports.sum = (a,b) => a + b;
-
-
-// module.exports = {
-//     sum
-// };
