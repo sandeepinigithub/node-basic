34c0f6098cdadc92f2df106c6c04986e82bbaad3
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2020-07-02 01:44:06 +0530

    8 Commit - node js core modules

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const {sum} = require("./helpers");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.end("Hello World from node js");
+})
+
+server.listen(3000);
 
 const total = sum(10 ,20); //earlier it was helpers.sum(10,20)
 console.log("TOTAL :",total);
