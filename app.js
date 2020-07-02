454a1e784e3b9aa4183f26357eb9cdbd6d845b63
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2020-07-02 10:16:05 +0530

    13 Commit - Nodejs Synchronous Programming

--- app.js
+++ app.js
@@ -10,11 +10,14 @@
 const fs = require("fs");
 const fileName = "target.txt";
 
-fs.readFile(fileName,(err, data) => {
-    if (err) {
-        conslose.log(err);
-    }
-    console.log(data.toString());
-});
+const data = fs.readFileSync(fileName);
+console.log(data.toString());
 
-console.log("Nodejs Asynchronous Programming"); // This should come at bottom of above data but above data takes more time ,thats why it got executed first.
+// fs.readFile(fileName,(err, data) => {
+//     if (err) {
+//         conslose.log(err);
+//     }
+//     console.log(data.toString());
+// });
+
+console.log("--->Nodejs Synchronous Programming"); 
